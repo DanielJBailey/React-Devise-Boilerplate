6cd02da7915a354f6e4088cf2952b59b92cbdf2d
Daniel Bailey <daniel.justin.bailey@gmail.com>
2019-01-04 22:25:04 -0700

    finish building out auth

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,19 @@ import Register from './components/Register';
 import NoMatch from './components/NoMatch';
 import {Switch, Route} from 'react-router-dom';
 import FetchUser from './components/FetchUser';
+import NavBar from './components/NavBar';
+import './App.scss';
 
 const App = () => (
   <>
+    <NavBar />
     <FetchUser>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/register" component={Register} />
-        <Route component={NoMatch} />
-      </Switch>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+          <Route component={NoMatch} />
+        </Switch>
     </FetchUser>
   </>
 )
